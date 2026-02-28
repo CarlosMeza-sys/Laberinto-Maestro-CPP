3b0b7fa571bfbc88569f930de3a29205e5579a23
Carlos Meza <cansas25@gmail.com>
2026-02-28 11:46:26 -0300

    feat: implementar algoritmo de generación por backtracking recursivo y lógica de caminos

--- laberinto.cpp
+++ laberinto.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <random>
+
+void generarLaberinto(std::vector<std::vector<char>>& laberinto, int filaActual, int columnaActual, int alto, int ancho) {
+    // La celda actual se marca en camino
+    laberinto[filaActual][columnaActual] = '*';
+    // Definición de las 4 direcciones
+    std::vector<std::pair<int, int>> direcciones = {{-2, 0}, {2, 0}, {0, -2}, {0, 2}};
+
+    // Mezclamos las direcciones aleatoriamente
+    std::random_device rd;  // Obtiene un numero aleatorio del hardware
+    std::mt19937 g(rd());   // Generador de números pseudoaleatorios
+    std::shuffle(direcciones.begin(), direcciones.end(), g);
+
+    // Intentamos cavar en cada una de las 4 direcciones mezcladas
+    for (auto dir : direcciones) {
+        int destinoFila = filaActual + dir.first;
+        int destinoColumna = columnaActual + dir.second;
+
+        // Validamos que el destino esté dentro de los límites del mapa y sea un '#'
+        if (destinoFila > 0 && destinoFila < alto - 1 && destinoColumna > 0 && destinoColumna < ancho - 1 && laberinto[destinoFila][destinoColumna] == '#') {
+            // Calcular la pared intermedia a demoler
+            int paredFila = filaActual + (dir.first / 2);
+            int paredColumna = columnaActual + (dir.second / 2);
+            // Demoler la pared intermedia
+            laberinto[paredFila][paredColumna] = '*';
+            // Recursividad: Se repite el proceso
+            generarLaberinto(laberinto, destinoFila, destinoColumna, alto, ancho);
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
     // Validación de entrada
@@ -12,14 +43,17 @@ int main(int argc, char* argv[]) {
     // Conversión de tipo de datos --- "IMPLEMENTAR ALGUNA LOGICA DE VALIDACION DE NUMEROS ENTEROS"
     int ancho = std::stoi(argv[1]);
     int alto = std::stoi(argv[2]);
-
     // Imprimir para confirmar datos correctos
-    std::cout << "Listo para generar la matriz de " <<ancho << "x" << alto << std::endl;
+    std::cout << "Matriz de " <<ancho << " x " << alto << std::endl;
 
     // Creación de la matriz dinámica.
     std::vector<std::vector<char>> laberinto(alto, std::vector<char>(ancho, '#'));
 
+    generarLaberinto(laberinto, 1, 1, alto, ancho);
+    laberinto[1][0] = '*';
+    laberinto[alto - 2][ancho - 1] = '*';
 
+    // Impresión de la matriz dinámica.
     for (int fila = 0; fila < alto; fila++) {
         for (int columna = 0; columna < ancho; columna++) {
             std::cout << laberinto[fila][columna];
